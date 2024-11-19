30edbcdfb7cf527562f8f421bed892f738591528
Gabriel Franco <82771295+francoggm@users.noreply.github.com>
2024-11-18 21:05:11 -0300

    use JPA to entities

--- src/main/java/projeto/gabriel/projeto/controller/CategoriaController.java
+++ src/main/java/projeto/gabriel/projeto/controller/CategoriaController.java
@@ -2,40 +2,52 @@ package projeto.gabriel.projeto.controller;
 
 import java.util.ArrayList;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
 import projeto.gabriel.projeto.model.Categoria;
+import projeto.gabriel.projeto.model.CategoriaRepository;
 
+@RestController
+@CrossOrigin(origins = "*")
+@AllArgsConstructor
 public class CategoriaController {
 
+    private CategoriaRepository repository;
+
     @PostMapping("/categoria")
-    public Categoria criarCategoria(Categoria c){
-        c.create();
-        return c;
+    public Categoria criarCategoria(Categoria categoria){
+        categoria.setId(null);
+        return repository.save(categoria);
     }
 
     @PutMapping("/categoria")
-    public Categoria atualizarCategoria(Categoria c) {
-        c.update();
-        return c;
+    public Categoria atualizarCategoria(Categoria categoria) {
+        if(categoria.getId() == null){
+            return null;
+        }
+        
+        return repository.save(categoria);
     }
 
     @DeleteMapping("/categoria")
-    public void deletarCategoria(Categoria c){
-        c.delete();
+    public void deletarCategoria(Categoria categoria){
+        repository.delete(categoria);
     }
 
     @GetMapping("/categoria")
     public ArrayList<Categoria> listarCategorias() {
-        return Categoria.getAll();
+        return (ArrayList<Categoria>) repository.findAll();
     }
 
     @GetMapping("/categoria/{id}")
-    public Categoria buscarCategoria(@PathVariable int id) {
-        return Categoria.get(id);
+    public Categoria buscarCategoria(@PathVariable long id) {
+        return repository.findById(id).get();
     }
 }
