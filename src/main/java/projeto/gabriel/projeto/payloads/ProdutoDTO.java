a74c918887921abff32fd43d847c2822c6f5a687
Gabriel <francogm77@hotmail.com>
2024-12-03 00:31:54 -0300

    adjust DTO

--- src/main/java/projeto/gabriel/projeto/payloads/ProdutoDTO.java
+++ src/main/java/projeto/gabriel/projeto/payloads/ProdutoDTO.java
@@ -2,6 +2,7 @@ package projeto.gabriel.projeto.payloads;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -24,6 +25,6 @@ public class ProdutoDTO {
   private List<Integer> categoriasIds = new ArrayList<>();
 
   public Produto toProduto() {
-    return new Produto(null, nome, valor, descricao, peso, null);
+    return new Produto(null, nome, valor, descricao, peso, new HashSet<>());
   }
 }
